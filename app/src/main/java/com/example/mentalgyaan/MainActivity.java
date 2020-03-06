a4ffd4f510d7290d70ee42cf2237c3c06e676066
Dhanam <parekhdhanam@gmail.com>
2020-03-06 16:56:20 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mentalgyaan/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.mentalgyaan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private BottomNavigationView bottomNavigationView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        InitializeFields();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TherapyFragment()).commit();
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bot_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+    }
+
+    //handle bottomnavigation buttons
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = new TherapyFragment();
+
+            switch (menuItem.getItemId()) {
+
+
+                case R.id.bot_therapy: {
+                    selectedFragment = new TherapyFragment();
+                    break;
+                }
+
+                case R.id.bot_chatbot: {
+
+                    break;
+                }
+                case R.id.bot_discover: {
+                    break;
+                }
+
+
+
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+
+            return true;
+
+        }
+
+
+    };
+    private void InitializeFields() {
+
+        SetupToolbar();
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bot_nav);
+
+
+    }
+
+    private void SetupToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.main_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("MentalGyaan");
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/mentalgyaan/TherapyFragment.java
@@ -0,0 +1,31 @@
+package com.example.mentalgyaan;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TherapyFragment extends Fragment {
+
+
+    public TherapyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_therapy, container, false);
+    }
+
+}
